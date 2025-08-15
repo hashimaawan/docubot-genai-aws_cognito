043604b760f0a356a16c484a77ba9193a44ffb52
hashimaawan <awanhashim800@gmail.com>
2025-08-15 12:12:33 +0500

    corrected apikey

--- app/utils/gemini_llm.py
+++ app/utils/gemini_llm.py
@@ -4,7 +4,7 @@ from config import GOOGLE_API_KEY
 def get_gemini_llm():
     return ChatGoogleGenerativeAI(
         model="gemini-1.5-flash",
-        google_api_key="AIzaSyBPTdGhPdRb1oN1uXiMi52-CJre2SQyia8",
+        google_api_key=GOOGLE_API_KEY,
         temperature=0.3
     )
 
